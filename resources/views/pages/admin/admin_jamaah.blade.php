17a7f6be445f3c35b707546c49b326651e2eb441
Muhammad Ripaldi <muhamadripaldi75@gmail.com>
2025-05-15 11:06:00 +0700

    rapihkan jamaah

--- resources/views/pages/admin/admin_jamaah.blade.php
+++ resources/views/pages/admin/admin_jamaah.blade.php
@@ -9,73 +9,8 @@
         <div class="d-flex">
             <!-- Main Content -->
             <div class="container-fluid p-4">
-                {{-- <div class="d-flex justify-content-end align-items-center mb-4">
-                    <div class="d-flex justify-content-end align-items-center mb-4">
-                        @include('pages.admin.partials.profile')
-                    </div>
-                </div> --}}
-
                 <h1 id="pageTitle" class="mb-4">Data Pemesanan</h1>
-
-
-
-                <!-- Modal Edit pesanan -->
-                <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="editModalLabel">Edit agen</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                    aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                <div class="mb-3">
-                                    <label for="editName" class="form-label">Name</label>
-                                    <input type="text" class="form-control" id="editName">
-                                </div>
-                                <div class="mb-3">
-                                    <label for="editEmail" class="form-label">Email</label>
-                                    <input type="email" class="form-control" id="editEmail">
-                                </div>
-                                <div class="mb-3">
-                                    <label for="editPhone" class="form-label">Phone</label>
-                                    <input type="text" class="form-control" id="editPhone">
-                                </div>
-                                <div class="mb-3">
-                                    <label for="editPaket" class="form-label">Paket</label>
-                                    <input type="text" class="form-control" id="editPaket">
-                                </div>
-                                <div class="mb-3">
-                                    <label for="editJumlah" class="form-label">Jumlah</label>
-                                    <input type="text" class="form-control" id="editJumlah">
-                                </div>
-                                <div class="mb-3">
-                                    <label for="editJemaah" class="form-label">Jemaah</label>
-                                    <input type="text" class="form-control" id="editJemaah">
-                                </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary" id="saveChanges">Save changes</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
                 <div class="bg-white p-4 shadow rounded">
-                    <!-- Search Field -->
-                    <div class="row mb-3">
-                        <div class="col-md-3">
-                            <div class="input-group">
-                                <span class="input-group-text" id="searchIcon"><i class="fas fa-search"></i></span>
-                                <input type="text" class="form-control" id="searchPaket"
-                                    placeholder="Search name, email, or etc">
-                            </div>
-                        </div>
-                    </div>
-
-
-
                     <table class="table table-bordered text-center">
                         <thead>
                             <tr>
@@ -196,8 +131,7 @@
                                                         <div class="col-md-6">
                                                             <label>Telepon</label>
                                                             <input type="text" name="telepon_pemesan"
-                                                                class="form-control"
-                                                                value="{{ $order->telepon_pemesan }}">
+                                                                class="form-control" value="{{ $order->telepon_pemesan }}">
                                                         </div>
                                                         <div class="col-md-12">
                                                             <label>Daftar Jamaah</label>
